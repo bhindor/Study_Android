c437e6bdcab090cd9395174b0787f5b78de51d0a
bhindor <82446373+bhindor@users.noreply.github.com>
2021-11-12 23:27:39 +0900

    2021/11/12
    RelativeLayout/TableLayout

---
+++ Ex03/app/src/main/java/kr/hs/ex/MainActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.ex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import kr.hs.ex.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
